77fe5c0fec65a56b2b81c9fd3dba51d5e28e33c5
Aaron Mead <aaronm14@gmail.com>
2014-07-30 23:18:25 -0400

    kind of useless commits here so far, but i scrapped what i was doing and decided to just start adding angular from the ground up, starting off in one file instead of worrying about the file structure for the time being.  just want to show data dynamically on the frontend, hope to accomplish this tomorrow

--- public/js/ang.js
+++ public/js/ang.js
@@ -1,3 +1,11 @@
-'use strict';
+var app = angular.module('photoBattleApp', []);
 
-angular.module('photoBattleApp', []);
+app.controller('VoteCtrl', function($scope) {
+  $scope.person = {
+    name: "Borey Borey"
+  };
+});
+
+app.controller('PhotoCtrl', ['$scope', function($scope) {
+  $scope.src = '/images/bachelor-juanpablo-nikki.jpg';
+}]);
--- public/js/services/photos.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('photoBattleApp').factory('Photos', ['$resource',
-  function($resource) {
-    return $resource('photos/:photoId', {
-      photoId: '@_id'
-    };
-  };
-]);
