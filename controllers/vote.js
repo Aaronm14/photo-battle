fafd4635e793404302f9828b9431f29cdab7231e
Aaron Mead <aaronm14@gmail.com>
2014-07-31 22:49:19 -0400

    checking out a few sites for making the data link into angular like http://programmaticponderings.wordpress.com/2014/03/24/retrieving-and-displaying-data-with-angularjs-and-the-mean-stack-part-ii-2/.  Made no progress but am falling asleep

--- controllers/vote.js
+++ controllers/vote.js
@@ -5,7 +5,7 @@
 var Challenge = require('../models/Challenge');
 var Photo = require('../models/Photo');
 
-var challenge = new Challenge({ subject: "bachelor" });
+/*var challenge = new Challenge({ subject: "bachelor" });
 challenge.save(function (err) {
   if (err) return console.error(err);
 });
@@ -15,7 +15,7 @@ photo.save(function (err) {
   if (err) return console.error(err);
 });
 
-/*Challenge.findOne({ subject: "bachelor" }).exec(function (err, challenge) {
+Challenge.findOne({ subject: "bachelor" }).exec(function (err, challenge) {
    if (err) return console.error(err);
 
    //add photo
@@ -26,5 +26,25 @@ photo.save(function (err) {
 exports.vote = function(req, res) {
   res.render('vote', {
     title: 'Voting'
+  });
+};
+module.exports = function (app) {
+  app.get('/api/components', function (req, res) {
+    Component.find(function (err, components) {
+      if (err)
+        res.send(err);
+
+      res.json(components);
+    });
+  });
+};
+module.exports = function (app) {
+    app.get('/api/components', function (req, res) {
+      Photo.find(function (err, photos) {
+        if (err)
+          res.send(err);
+
+        res.json(photos);
+      });
   });
 };
